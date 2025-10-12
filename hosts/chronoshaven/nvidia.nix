b85f0b640d520385e60904b928cd270cdf8c0236
Shomy 🍂 <61943525+shomykohai@users.noreply.github.com>
2025-10-12 18:16:53 +0200

    hosts(ch): Drop nvidia drivers
    It just caused more problems for some reason than not, like lag spikes and crashes.
    Just rely on intel iGPU instead

--- hosts/chronoshaven/nvidia.nix
+++ hosts/chronoshaven/nvidia.nix
@@ -1,35 +1,38 @@
-{ pkgs, config, libs, ... }:
-
 {
+  pkgs,
+  config,
+  libs,
+  ...
+}: {
   # OpenGL
   hardware.graphics = {
     enable = true;
   };
 
-
-  services.xserver.videoDrivers = ["nvidia" "intel"];
+  services.xserver.videoDrivers = [
+    # "nvidia"
+    "intel"
+  ];
 
   # Stop nix from complaining
 
-  nixpkgs.config.nvidia.acceptLicense = true;
+  # nixpkgs.config.nvidia.acceptLicense = true;
 
-  hardware.nvidia = {
-    modesetting.enable = true;
-    open = false;
+  # hardware.nvidia = {
+  #   modesetting.enable = true;
+  #   open = false;
 
+  #   powerManagement.enable = false;
+  #   powerManagement.finegrained = false;
 
-    powerManagement.enable = false;
-    powerManagement.finegrained = false;
+  #   prime = {
+  #     intelBusId = "PCI:00:02.0";
+  #     nvidiaBusId = "PCI:01:00.0";
+  #   };
 
-    prime = {
-      intelBusId = "PCI:00:02.0";
-      nvidiaBusId = "PCI:01:00.0";
-    };
-    
-    # On wayland, the display is not recognized anyway
-    nvidiaSettings = false;
-    
-    package = config.boot.kernelPackages.nvidiaPackages.legacy_470;
-  };
+  #   # On wayland, the display is not recognized anyway
+  #   nvidiaSettings = false;
 
+  #   package = config.boot.kernelPackages.nvidiaPackages.legacy_470;
+  # };
 }
