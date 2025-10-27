fb534c30df8ea0e474baf937507e6bab7b074ef0
Shomy 🍂 <61943525+shomykohai@users.noreply.github.com>
2025-10-28 00:44:53 +0100

    users(shomy): Configure ghostty
    Rose Pine'd too

--- users/shomy/programs/ghostty.nix
+++ users/shomy/programs/ghostty.nix
@@ -1,3 +1,15 @@
 {pkgs, ...}: {
   environment.systemPackages = [pkgs.ghostty];
+
+  homix.".config/ghostty/config" = {
+    text = ''
+      theme = rose-pine-moon
+      shell-integration = zsh
+
+      bold-is-bright = "true"
+
+      keybind = ctrl+shift+r=reload_config
+    '';
+    copy = true;
+  };
 }
