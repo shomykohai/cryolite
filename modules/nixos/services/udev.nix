b3bdddbd734671a41821281eaa3c9ad309131280
Shomy 🍂 <61943525+shomykohai@users.noreply.github.com>
2025-12-19 20:18:31 +0100

    modules(nixos): Add MTK Udev rules
    Have to figure out how to avoid needing this

--- modules/nixos/services/udev.nix
+++ modules/nixos/services/udev.nix
@@ -1,5 +1,9 @@
 {frostix, ...}: {
   services.udev = {
     packages = [frostix.mtkclient-git];
+    extraRules = ''
+      SUBSYSTEM=="usb", ATTRS{idVendor}=="0e8d", ATTRS{idProduct}=="2000", MODE="0660", GROUP="dialout"
+      SUBSYSTEM=="usb", ATTRS{idVendor}=="0e8d", ATTRS{idProduct}=="0003", MODE="0660", GROUP="dialout"
+    '';
   };
 }
