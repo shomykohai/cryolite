7613bdd6fe63e5cb32c752bf5c89188efb725ef7
Shomy 🍂 <61943525+shomykohai@users.noreply.github.com>
2025-12-19 22:12:37 +0100

    hosts(tc): Allocate 8GB for zram

--- hosts/temporalcatalyst/configuration.nix
+++ hosts/temporalcatalyst/configuration.nix
@@ -1,11 +1,7 @@
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
-{
-  config,
-  pkgs,
-  ...
-}: {
+{...}: {
   imports = [
     # Import default configuration first
     ../../modules/nixos/default-configuration.nix
@@ -19,6 +15,10 @@
   ];
 
   networking.hostName = "temporalcatalyst";
+
+  zramSwap = {
+    memoryPercent = 1024 * 8; # 8GB
+  };
 
   system.desktopEnvironment = "KDE";
   docker.stregatto.enable = true;
