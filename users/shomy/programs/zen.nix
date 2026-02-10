16ac1acb260259e839ff40333f4cbb67971ce5f5
Shomy 🍂 <61943525+shomykohai@users.noreply.github.com>
2026-02-10 03:43:19 +0100

    users(shomy): Add BitWarden to Zen Browser

--- users/shomy/programs/zen.nix
+++ users/shomy/programs/zen.nix
@@ -77,6 +77,11 @@
         # To get GUID -> https://addons.mozilla.org/api/v5/addons/addon/<addon-slug>/
         ExtensionSettings = {
           "*".installation_mode = "blocked";
+          "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
+            installation_mode = "force_installed";
+            private_browsing = true;
+          };
           "uBlock0@raymondhill.net" = {
             install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
             installation_mode = "force_installed";
