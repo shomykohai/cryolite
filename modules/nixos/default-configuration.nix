5f5a13f3b7b464d6243f3fa40339dea6afae2309
shomy <git@itssho.my>
2026-04-08 00:41:03 +0200

    modules(nixos): Import root user

--- modules/nixos/default-configuration.nix
+++ modules/nixos/default-configuration.nix
@@ -1,17 +1,8 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
 {
   lib,
   config,
   ...
 }: {
-  # options.system.users = lib.mkOption {
-  #   type = lib.types.listOf lib.types.str;
-  #   default = [];
-  #   description = "List of usernames to configure.";
-  # };
-
   # Default shared settings between hosts
   imports = [
     ./boot
@@ -26,6 +17,8 @@
 
     ../containers/docker.nix
     ../games/default.nix
+
+    ../../users/root
   ];
 
   environment.variables = {
