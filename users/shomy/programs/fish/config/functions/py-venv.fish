9139a890f846f469e58d56a40991543a11ff88ad
shomy <git@itssho.my>
2026-05-22 15:51:16 +0200

    users(shomy): Fix py-venv fish command

--- users/shomy/programs/fish/config/functions/py-venv.fish
+++ users/shomy/programs/fish/config/functions/py-venv.fish
@@ -1,3 +1,3 @@
-function py-venv --wraps='python -m venv .venv && source .venv/bin/activate' --description 'alias py-venv=python -m venv .venv && source .venv/bin/activate'
-    python -m venv .venv && source .venv/bin/activate $argv
+function py-venv --wraps='python -m venv .venv && source .venv/bin/activate.fish' --description 'alias py-venv=python -m venv .venv && source .venv/bin/activate.fish'
+    python -m venv .venv && source .venv/bin/activate.fish $argv
 end
