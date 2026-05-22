2f19d6d0913c0e6469066ddb075fb2b274a759ea
shomy <git@itssho.my>
2026-05-22 15:49:26 +0200

    modules(nixos): Speed up boot times

--- modules/nixos/boot/bootloader.nix
+++ modules/nixos/boot/bootloader.nix
@@ -1,7 +1,13 @@
-{...}: {
+{lib, ...}: {
   boot.loader.limine = {
     enable = true;
     maxGenerations = 5;
   };
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.timeout = 0;
+  boot.initrd.verbose = false;
+
+  systemd = {
+    network.wait-online.enable = lib.mkDefault false;
+  };
 }
