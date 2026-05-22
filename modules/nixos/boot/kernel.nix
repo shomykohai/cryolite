e1b9108e2ff9ffa0f1971d982ac894b0f2549914
shomy <git@itssho.my>
2026-05-22 15:49:44 +0200

    modules(nixos): Add plymouth splash

--- modules/nixos/boot/kernel.nix
+++ modules/nixos/boot/kernel.nix
@@ -4,4 +4,10 @@
   ...
 }: {
   boot.kernelPackages = lib.mkDefault pkgs.linuxPackages_latest;
+  boot.kernelParams = [
+    "quiet"
+    "splash"
+    "udev.log_level=3"
+    "nowatchdog"
+  ];
 }
--- modules/nixos/boot/plymouth.nix
+++ modules/nixos/boot/plymouth.nix
@@ -1,1 +1,9 @@
-{...}: {}
+{pkgs, ...}: {
+  boot.plymouth = {
+    enable = true;
+    theme = "bgrt";
+    themePackages = [
+      pkgs.nixos-bgrt-plymouth
+    ];
+  };
+}
