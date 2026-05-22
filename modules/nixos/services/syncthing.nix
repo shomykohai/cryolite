d4d117b7ff131ab165afe39f125dd7f5af3f24ac
shomy <git@itssho.my>
2026-05-22 15:50:23 +0200

    modules(nixos): Make syncthing service start later

--- modules/nixos/services/syncthing.nix
+++ modules/nixos/services/syncthing.nix
@@ -1,4 +1,8 @@
-{...}: {
+{
+  lib,
+  config,
+  ...
+}: {
   ## TODO: Uncouple syncthing from my user account, and make it configurable per-user basis
   services.syncthing = {
     user = "shomy";
@@ -7,5 +11,16 @@
     overrideDevices = true;
     overrideFolders = true;
     openDefaultPorts = true;
+  };
+
+  systemd.services.syncthing-init = {
+    wantedBy = lib.mkForce (
+      if config.system.desktopEnvironment != "none"
+      then ["graphical.target"]
+      else ["multi-user.target"]
+    );
+    after = lib.mkForce (
+      ["syncthing.service"] ++ lib.optionals (config.system.desktopEnvironment != "none") ["graphical.target"]
+    );
   };
 }
