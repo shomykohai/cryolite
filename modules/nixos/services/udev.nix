2ce8635a105e0432358979c17af3c8adc3c04168
shomy <git@itssho.my>
2026-08-06 17:58:12 +0200

    modules(nixos): Maybe fix mtk udev rules

--- modules/nixos/services/udev.nix
+++ modules/nixos/services/udev.nix
@@ -2,8 +2,9 @@
   services.udev = {
     packages = [frostix.mtkclient-git];
     extraRules = ''
-      SUBSYSTEM=="usb", ATTRS{idVendor}=="0e8d", ATTRS{idProduct}=="2000", MODE="0660", GROUP="dialout"
-      SUBSYSTEM=="usb", ATTRS{idVendor}=="0e8d", ATTRS{idProduct}=="0003", MODE="0660", GROUP="dialout"
+      SUBSYSTEM=="usb", ATTRS{idVendor}=="0e8d", ATTRS{idProduct}=="0003", TAG+="uaccess"
+      SUBSYSTEM=="usb", ATTRS{idVendor}=="0e8d", ATTRS{idProduct}=="2000", TAG+="uaccess"
+      SUBSYSTEM=="usb", ATTRS{idVendor}=="0e8d", ATTRS{idProduct}=="2001", TAG+="uaccess"
     '';
   };
 }
