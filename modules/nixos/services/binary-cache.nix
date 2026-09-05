8323e1a6a8301a692f3748911cfeceec17b9c460
shomy <git@itssho.my>
2026-09-05 09:26:01 +0200

    modules(nixos): Add `sunset` binary cache

--- modules/nixos/services/binary-cache.nix
+++ modules/nixos/services/binary-cache.nix
@@ -15,8 +15,9 @@
   nix.settings = {
     substituters = [
       "https://attic.services.itssho.my/sunrise"
+      "https://attic.services.itssho.my/sunset"
     ];
-    trusted-public-keys = ["sunrise:hogFc/tNDw5cXhdBfFagDNEEiR6NGspXBzyVJhzka/4="];
+    trusted-public-keys = ["sunrise:/7pI1oAoU8oO1b5DiJIrv/V6oYeUE8kfD2t2sgeti98=" "sunset:S+4aNsqbQNAvBfSLS/q/dmiOY7MK272I2AEOFqmrvs0="];
     netrc-file = config.sops.templates.netrc.path;
   };
 }
